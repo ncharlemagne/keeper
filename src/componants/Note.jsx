ab99ca8937db01942efc4c3a462e2a446d92c091
ncharlemagne <125274331+ncharlemagne@users.noreply.github.com>
2023-09-08 14:19:45 +0100

    Initial commit

---
+++ src/componants/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is the sample text Heading to be applied to the page.</h1>
+      <p>
+        This is the content which be applied to the page and styled via the css
+        file.
+      </p>
+    </div>
+  );
+}
+export default Note;
